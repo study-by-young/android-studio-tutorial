7934060114e7d51169a3e5c7b26a24b06580c3e7
foreveryoun910 <foreveryoun910@gmail.com>
2023-11-01 22:46:57 +0900

    Stopwatch 메인 액티비티 구현

--- Stopwatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ Stopwatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,10 +2,94 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import java.util.Timer
+import kotlin.concurrent.timer
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private var timer : Timer? = null
+    private var time = 0
+
+    private lateinit var btn_start : Button // lateinint : 나중에 초기화
+    private lateinit var btn_refresh : Button
+    private lateinit var tv_minute : TextView
+    private lateinit var tv_second : TextView
+    private lateinit var tv_millisecond : TextView
+
+    private var isRunning = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_start = findViewById(R.id.btn_start)
+        btn_refresh = findViewById(R.id.btn_refresh)
+        tv_minute = findViewById(R.id.tv_minute)
+        tv_second = findViewById(R.id.tv_second)
+        tv_millisecond = findViewById(R.id.tv_millisecond)
+
+        btn_start.setOnClickListener(this)
+        btn_refresh.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id) {
+            R.id.btn_start -> {
+                if(isRunning) {
+                    pause()
+                } else {
+                    start()
+                }
+            }
+            R.id.btn_refresh -> {
+                refresh()
+            }
+        }
+    }
+
+    private fun start() {
+        btn_start.text = getString(R.string.btn_pause)
+        btn_start.setBackgroundColor(getColor(R.color.btn_pause))
+        isRunning = true
+
+        timer = timer(period = 10) { // 10ms 마다 timer 함수가 돈다
+            // 1000ms = 1s
+            // 0.01 time 1++
+            time++
+
+            val milli_second = time % 100
+            val second = (time % 6000) / 100
+            val minute = time / 6000
+
+            runOnUiThread {
+                if(isRunning) {
+                    tv_millisecond.text = if(milli_second < 10) ".0${milli_second}" else ".${milli_second}"
+                    tv_second.text = if(second < 10) ":0${second}" else ":${second}"
+                    tv_minute.text = "${minute}"
+                }
+            }
+        }
+    }
+
+    private fun pause() {
+        btn_start.text = getString(R.string.btn_start)
+        btn_start.setBackgroundColor(getColor(R.color.btn_start))
+        isRunning = false
+        timer?.cancel() // 타이머 멈춤
+    }
+
+    private fun refresh() {
+        timer?.cancel()
+        btn_start.text = getString(R.string.btn_start)
+        btn_start.setBackgroundColor(getColor(R.color.btn_start))
+        isRunning = false
+
+        time = 0
+        tv_millisecond.text = ".00"
+        tv_second.text = ":00"
+        tv_minute.text = "00"
     }
 }
