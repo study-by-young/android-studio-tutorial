2ac02ae9c415607a6ae4b041ae0288955b823109
foreveryoun910 <foreveryoun910@gmail.com>
2023-11-14 22:34:57 +0900

    레트로핏 라이브러리 설정: 레트로핏 클래스, HTTP 메서드 정의 인터페이스 생성

---
+++ AirQuality/app/src/main/java/com/young/airquality/retrofit/RetrofitConnection.kt
@@ -0,0 +1,21 @@
+package com.young.airquality.retrofit
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitConnection {
+    companion object {
+        private const val BASE_URL = "https://api.airvisual.com/v2/"
+        private var INSTANCE : Retrofit? = null
+
+        fun getInstance() : Retrofit {
+            if(INSTANCE == null) {
+                INSTANCE = Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create()) // 서버에서 받은 JSON 응답을 데이터 클래스 객체로 변환
+                    .build()
+            }
+            return INSTANCE!!
+        }
+    }
+}
