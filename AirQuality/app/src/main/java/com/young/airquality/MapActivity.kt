2a72d1f5c5b44a93939e05edb7f2a4e6465d162d
foreveryoun910 <foreveryoun910@gmail.com>
2023-11-16 21:18:48 +0900

    맵 액티비티 구현

--- AirQuality/app/src/main/java/com/young/airquality/MapActivity.kt
+++ AirQuality/app/src/main/java/com/young/airquality/MapActivity.kt
@@ -1,11 +1,86 @@
 package com.young.airquality
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.young.airquality.databinding.ActivityMapBinding
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    lateinit var binding : ActivityMapBinding
+
+    private var mMap : GoogleMap? = null
+    var currentLat : Double = 0.0
+    var currentLng : Double = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_map)
+        binding = ActivityMapBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        currentLat = intent.getDoubleExtra("currentLat", 0.0)
+        currentLng = intent.getDoubleExtra("currentLng", 0.0)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this) // 지도 준비 요청
+
+        setButton()
+    }
+
+    private fun setButton() {
+        binding.btnCheckHere.setOnClickListener {
+            mMap?.let {
+                val intent = Intent()
+                intent.putExtra("latitude", it.cameraPosition.target.latitude)
+                intent.putExtra("longitude", it.cameraPosition.target.longitude)
+                setResult(Activity.RESULT_OK, intent)
+                finish() // Activity 종료
+            }
+        }
+
+        binding.fabCurrentLocation.setOnClickListener {
+            val locationProvider = LocationProvider(this@MapActivity)
+            val latitude = locationProvider.getLocationLatitude()
+            val longitude = locationProvider.getLocationLongitude()
+
+            mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(latitude!!, longitude!!), 16f))
+            setMarker()
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+
+        mMap = googleMap
+
+        mMap?.let {
+            val currentLocation = LatLng(currentLat, currentLng)
+            it.setMaxZoomPreference(20.0f)
+            it.setMinZoomPreference(12.0f)
+            it.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 16f))
+            setMarker()
+        }
+    }
+
+    private fun setMarker() {
+        mMap?.let { // mMap 이 null 이 아닌 경우에만 let 내부 실행
+            it.clear()
+            val markerOption = MarkerOptions()
+            markerOption.position(it.cameraPosition.target)
+            markerOption.title("마커 위치")
+            val marker = it.addMarker(markerOption)
+
+            it.setOnCameraMoveListener { // 지도를 따라서 마크가 이동
+                marker?.let { marker ->
+                    marker.position = it.cameraPosition.target
+                }
+            }
+        }
     }
 }
